f34782ca00e4b889bcdfe1ad66b04145aed0f5d4
arch mage <softwarearchaeologist@gmail.com>
2022-05-16 12:31:42 +0530

    initial commit

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -18,13 +18,6 @@ const LayoutWrapper = ({ children }) => {
                 <div className="mr-3">
                   <Logo />
                 </div>
-                {typeof siteMetadata.headerTitle === 'string' ? (
-                  <div className="hidden h-6 text-2xl font-semibold sm:block">
-                    {siteMetadata.headerTitle}
-                  </div>
-                ) : (
-                  siteMetadata.headerTitle
-                )}
               </div>
             </Link>
           </div>
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
+  title: 'Blockata',
+  author: 'Arch Mage',
+  headerTitle: '',
+  description: 'Defi and Blockchain Tutorials',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
